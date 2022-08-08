beaf7fd16f064067a903197fb9871bb79acac074
masud <abdullahalmasud01737906772@gmail.com>
2022-08-09 00:24:34 +0600

    added ssd spiker ram monitor printer

--- index.js
+++ index.js
@@ -31,51 +31,114 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        /* --------------Start Monitor Collection---------------- */
+        const MonitorDellCollection = client.db('Monitor').collection('MonitorDell');
+        /* --------------end Monitor Collection---------------- */
+
         /* --------------Start Component Collection---------------- */
         const ComponentsPorcessorIntelCollection = client.db('Components').collection('IntelProcessor');
         const ComponentsProcessorAMDCollection = client.db('Components').collection('AMDProcessor');
+        const ComponentsRamAdataCollection = client.db('Components').collection('AdataRam');
         /* --------------End Component Collection---------------- */
 
         /* --------------Start Networking Collection---------------- */
         const NetworkingRouterTPLinkCollection = client.db('Networking').collection('TPLinkRouter');
         /* --------------End Networking Collection------------------ */
 
+        /* --------------Start Office Equipment Collection------------------ */
+        const OfficeEquipmentPrinterHPCollection = client.db('OfficeEquipment').collection('PrinterHP');
+        /* --------------End Office Equipment Collection------------------ */
+
         /* --------------Start Accessories Collection---------------- */
         const AccessoriesKeyboardMicropackCollection = client.db('Accessories').collection('KeyboardMicropack');
         /* --------------End Acceessories Collection---------------- */
-        
+
         /* --------------Start Security Collection---------------- */
         const SecurityCcCameraHikvisionCollection = client.db('Security').collection('CcCameraHikvision');
         /* --------------End Security Collection---------------- */
 
-        // Components/Processor/Intel
-         app.get('/intels', async (req, res) =>{
-             const query = {};
-             const cursor = ComponentsPorcessorIntelCollection.find(query);
-             const Intels = await cursor.toArray();
-             res.send(Intels);
-         });
+        /* --------------Start Storage Collection---------------- */
+        const StorageSSDHPCollection = client.db('Storage').collection('SSDHP');
+        /* --------------End Storage Collection---------------- */
 
-         app.get('/intels/:id', async (req, res) =>{
-             const id = req.params.id;
-             const query = {_id: ObjectId(id)};
-             const Intel = await ComponentsPorcessorIntelCollection.findOne(query);
-             res.send(Intel);
-         })
+        /* --------------Start Storage Collection---------------- */
+        const SoundSystemSpeakerDigitalXCollection = client.db('SoundSystem').collection('SpeakerDigitalX');
+        /* --------------End Storage Collection---------------- */
+
+
+        // Monitor/Dell-0
+        app.get('/monitor-dells', async (req, res) => {
+            const query = {};
+            const cursor = MonitorDellCollection.find(query);
+            const MonitorDells = await cursor.toArray();
+            res.send(MonitorDells);
+        });
+
+        app.get('/monitor-dells/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const MonitorDell = await MonitorDellCollection.findOne(query);
+            res.send(MonitorDell);
+        })
+
+        // Components/Processor/Intel
+        app.get('/intels', async (req, res) => {
+            const query = {};
+            const cursor = ComponentsPorcessorIntelCollection.find(query);
+            const Intels = await cursor.toArray();
+            res.send(Intels);
+        });
+
+        app.get('/intels/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const Intel = await ComponentsPorcessorIntelCollection.findOne(query);
+            res.send(Intel);
+        });
+
+        //Components/Ram/Adata-0
+        app.get('/ram-adatas', async (req, res) =>{
+            const query = {};
+            const cursor = ComponentsRamAdataCollection.find(query);
+            const RamAdatas = await cursor.toArray();
+            res.send(RamAdatas);
+        });
+
+        app.get('/ram-adatas/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query ={_id: ObjectId(id)};
+            const RamAdata = await ComponentsRamAdataCollection.findOne(query);
+            res.send(RamAdata);
+        })
 
         // Networking/Router/TPLink
-        app.get('/tplinks', async (req, res) =>{
+        app.get('/tplinks', async (req, res) => {
             const query = {};
             const cursor = NetworkingRouterTPLinkCollection.find(query);
             const TPLinks = await cursor.toArray();
             res.send(TPLinks);
         });
 
-        app.get('/tplinks/:id', async (req, res) =>{
+        app.get('/tplinks/:id', async (req, res) => {
             const id = req.params.id;
-            const query ={_id: ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const TPLink = await NetworkingRouterTPLinkCollection.findOne(query);
             res.send(TPLink);
+        });
+
+        // Office Equipment/Printer/HP-0
+        app.get('/printer-hps', async (req, res) => {
+            const query = {};
+            const cursor = OfficeEquipmentPrinterHPCollection.find(query);
+            const PrinterHPs = await cursor.toArray();
+            res.send(PrinterHPs);
+        });
+
+        app.get('/printer-hps/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const PrinterHP = await OfficeEquipmentPrinterHPCollection.findOne(query);
+            res.send(PrinterHP);
         })
 
         // Components/Processor/AMD
@@ -113,17 +176,46 @@ async function run() {
         app.get('/micropacks', async (req, res) => {
             const query = {};
             const cursor = AccessoriesKeyboardMicropackCollection.find(query);
-            const micropacks = await cursor.toArray();
-            res.send(micropacks);
+            const keyboardMicropacks = await cursor.toArray();
+            res.send(keyboardMicropacks);
         });
 
         app.get('/micropacks/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const micropack = await AccessoriesKeyboardMicropackCollection.findOne(query);
-            res.send(micropack);
+            const keyboardMicropack = await AccessoriesKeyboardMicropackCollection.findOne(query);
+            res.send(keyboardMicropack);
         });
 
+        // Storage/SSD/HP-0
+        app.get('/ssd-hps', async (req, res) => {
+            const query = {};
+            const cursor = StorageSSDHPCollection.find(query);
+            const SSDHPs = await cursor.toArray();
+            res.send(SSDHPs);
+        });
+
+        app.get('/ssd-hps/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const SSDHP = await StorageSSDHPCollection.findOne(query);
+            res.send(SSDHP);
+        });
+
+        //Sound system/Speaker/Digital-X-0
+        app.get('/speaker-digital-Xs', async (req, res) => {
+            const query = {};
+            const cursor = SoundSystemSpeakerDigitalXCollection.find(query);
+            const SpeakerDigitalXs = await cursor.toArray();
+            res.send(SpeakerDigitalXs);
+        });
+
+        app.get('/speaker-digital-Xs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const SpeakerDigitalX = await SoundSystemSpeakerDigitalXCollection.findOne(query);
+            res.send(SpeakerDigitalX);
+        })
 
 
     }
